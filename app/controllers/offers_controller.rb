085ba328e4ee96e771b4090acfd70e021eee6af1
Alberto Rocha <betogrun@gmail.com>
2024-02-24 18:20:05 -0300

    Add pagination

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,6 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.by_demographics(current_player.gender, current_player.age)
+    @pagy, @offers = pagy(Offer.by_demographics(current_player.gender, current_player.age), items: 10)
   end
 
   def show
