8c8d3ad40d94005344d56a706071151840048edf
Alberto Rocha <betogrun@gmail.com>
2024-02-24 22:39:50 -0300

    Update offers query and add tests

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,9 +2,12 @@ class Offer < ApplicationRecord
   has_many :claimed_offers
   has_many :players, through: :claimed_offers
 
+  scope :not_claimed_by, lambda { |player|
+    where.not(id: ClaimedOffer.where(player: player).pluck(:offer_id))
+  }
   scope :by_demographics, lambda { |gender, age|
     where(
-      'target_gender = :gender OR target_gender = \'all\' AND target_range_age @> :age',
+      '(target_gender = :gender OR target_gender = \'all\') AND target_range_age @> :age',
        gender: gender, age: age
     )
   }
