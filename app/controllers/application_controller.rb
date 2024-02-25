72379e4e0504f90dcf3ef070cd54893cad70515d
Alberto Rocha <betogrun@gmail.com>
2024-02-25 14:05:11 -0300

    Update strategy to clear unnecessary flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :authenticate_player! 
-  before_action :clear_post_signout_alerts
-
-  private
-
-  def clear_post_signout_alerts
-    flash.delete(:alert) if session.delete(:sign_out)
-  end
 end
--- app/controllers/player/sessions_controller.rb
+++ app/controllers/player/sessions_controller.rb
@@ -7,15 +7,10 @@ class Player
       flash.clear
     end
 
-    def destroy
-      super
-      session[:sign_out] = true
-    end
-
     private
 
     def clear_alerts_for_root_path
-      flash.delete(:alert) if request.referer.nil? || request.referer.ends_with?(root_url) 
+      flash.delete(:alert)
     end
   end
 end
